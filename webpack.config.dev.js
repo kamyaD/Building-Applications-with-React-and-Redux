3e26814bb85c625b6675a72ba3f72d12dff506ff
Kamya Domnick <kamya.domnick@andela.com>
2019-06-07 11:42:13 +0300

    Complete environment setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWbpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWbpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|j)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)&/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
